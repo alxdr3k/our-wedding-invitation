8e68079ef7f9a32d02b26f89a84d8fc932dfca96
alxdr3k <alxdr3k@gmail.com>
2025-07-06 16:54:04 +0900

    add condition for kakaopay icon

--- src/components/account.svelte
+++ src/components/account.svelte
@@ -68,9 +68,11 @@
                         </div>
                         <div class="account-row">
                             <span class="account-value">{account.bank}</span>
+                            {#if account.url}
                             <button class="kakaopay-btn {localeStore.locale}" on:click={() => openKakaoPay(account.url)}>
                                 <img src="payment_icon_yellow_small.png" alt="KakaoPay icon" class="kakaopay-image-icon" />
                             </button>
+                            {/if}
                         </div>
                         <div class="account-row">
                             <span class="account-value">{account.number}</span>
